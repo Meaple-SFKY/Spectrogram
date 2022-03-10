bbaf8fc0f294aac043b4033b2bb8e67cdcb4ec43
sfky <meaple.sfky@icloud.com>
2022-03-10 16:13:35 +0800

    pre-model

--- src/data_pre.py
+++ src/data_pre.py
@@ -3,6 +3,7 @@ import torch
 import torchaudio
 import matplotlib.pyplot as plt
 import utils
+import cv2 as cv
 
 from tqdm import tqdm
 
@@ -13,10 +14,11 @@ audio_path = './audio/'
 data_path = './data/'
 doc_path = './docs/'
 img_path = './image/'
-mdl_path = './model'
+mdl_path = './model/'
 src_path = './src/'
 log_errPath = './logs/dataPreErr.log'
 log_infoPath = './logs/dataPreInfo.log'
+log_warnPath = './logs/dataPreWarn.log'
 dataClasses = ['dev/', 'test/', 'train/']
 source_path = '/Volumes/External/Speaker-Rec/Dataset/'
 
@@ -79,7 +81,18 @@ def SpecGenerator(dataDocDirs):
 				dataDocFile.close()
 	plt.close()
 
+def RGBToGray():
+	for root, dirs, names in os.walk(img_path):
+		className = os.path.join(data_path + root[-5:])
+		if not os.path.exists(className) and className[-5] == 'S':
+			os.mkdir(className)
+			for name in tqdm(names, desc=className):
+				fileName = os.path.join(root, name)
+				image = cv.imread(fileName, cv.IMREAD_GRAYSCALE)
+				cv.imwrite(os.path.join(className, os.path.basename(name)), image)
+
 if __name__ == '__main__':
 	# proFilePath(source_path, doc_path)
 	# SpecGenerator(dataDocDirs=dataClasses)
-	print("OK")
+	# RGBToGray()
+	print('OK')
